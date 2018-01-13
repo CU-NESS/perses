fa8ea322bd1f7472f0075a77fde3778e634ba470
ktausch <Keith.Tauscher@colorado.com>
2018-01-13 10:53:30 -0700

    Added perses/util/AresSignal.py which has function which takes in kwargs and frequencies and outputs ares signal.

---
+++ perses/util/AresSignal.py
@@ -0,0 +1,24 @@
+"""
+"""
+import numpy as np
+from ares.simulations.Global21cm import Global21cm
+
+def ares_signal(frequencies, in_Kelvin=False, **kwargs):
+    """
+    
+    """
+    sim = Global21cm(**kwargs)
+    sim.run()
+    nu = 1420.41 / (1 + sim.history['z'])
+    signal = np.interp(frequencies, nu, sim.history['dTb'])
+    if in_Kelvin:
+        return signal / 1e3
+    else:
+        return signal
+
+def combine_dicts(dict1, dict2):
+    """
+    """
+    new_dict = dict1.copy()
+    new_dict.update(dict2)
+    return new_dict
