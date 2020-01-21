Fixed a bug in DriftscanSetCreator that would cause loading it from an hdf5 file to yield wrong results. ; Modified IsotropicBeam class so that isotropic beams over 2pi sr can be included as easily as isotropic beams over 4pi sr. This allows for better simulation of Earth-based experiments because sources below the horizon are not visible. ; Changed total power GridMeasuredBeam class so that grids are converted to maps of a given nside parameter only once instead of every time they are desired. The same thing will be done with the polarized GridMeasuredBeam class soon. ; Added ToyGalaxy class to perses.foregrounds. It represents a galaxy that is only one temperature away from the galactic plane. The galactic plane has a width in theta which can be a function of phi and the temperature in the plane can also be a temperature of phi.

perses/beam/total_power/GridMeasuredBeam.py
@@ -7,9 +7,8 @@ from pylinex import Loadable, Savable, create_hdf5_dataset, get_hdf5_value
 from ...util import ParameterFile, real_numerical_types, sequence_types,\
     spherical_harmonic_fit, reorganize_spherical_harmonic_coefficients,\
     quintic_spline_real
-from ..BeamUtilities import rotate_map, rotate_maps, convolve_grid,\
-    convolve_grids, normalize_grids, normalize_maps, grids_from_maps,\
-    symmetrize_grid, maps_from_grids, spin_grids, smear_grids
+from ..BeamUtilities import normalize_grids, normalize_maps, grids_from_maps,\
+    symmetrize_grid, maps_from_grids, spin_grids, rotate_maps
 from ..BaseBeam import DummyPool
 from .BaseTotalPowerBeam import _TotalPowerBeam
 
@@ -237,6 +236,30 @@ class GridMeasuredBeam(_TotalPowerBeam, Savable, Loadable):
             self._grids = symmetrize_grid(value)
         else:
             self._grids = value
+    
+    @property
+    def maps(self):
+        """
+        Property storing a dictionary whose keys are nside resolution
+        parameters and whose values are arrays of the shape
+        (len(self.frequencies), npix) where npix is the number of pixels
+        associated with the nside resolution parameter key.
+        """
+        if not hasattr(self, '_maps'):
+            self._maps = {}
+        return self._maps
+    
+    def make_maps(self, nside):
+        """
+        Translates the data grids to maps internally for the given resolution.
+        This function does nothing if this translation has already been done.
+        Once this function has been called, the maps are in self.maps[nside].
+        
+        nside: the resolution parameter for the healpy maps. Must be power of 2
+        """
+        if nside not in self.maps:
+            self.maps[nside] = maps_from_grids(self.grids, nside,\
+                theta_axis=-2, phi_axis=-1)
 
     def get_maps(self, frequencies, nside, pointing, psi, normed=True):
         """
@@ -253,33 +276,27 @@ class GridMeasuredBeam(_TotalPowerBeam, Savable, Loadable):
                 the form of a 2D numpy array of shape (nfreqs, npix) if a
                 sequence of frequencies is given
         """
-        if type(frequencies) in real_numerical_types:
-            frequencies = [1.*frequencies]
-            multi_frequency = False
-        else:
-            multi_frequency = True
-        p_theta = 90. - pointing[0]
-        p_phi = pointing[1]
+        self.make_maps(nside)
+        frequencies_originally_single_number =\
+            (type(frequencies) in real_numerical_types)
+        if frequencies_originally_single_number:
+            frequencies = [1. * frequencies]
         numfreqs = len(frequencies)
         npix = hp.pixelfunc.nside2npix(nside)
         maps = np.ndarray((numfreqs, npix))
-        for ifreq in range(numfreqs):
+        for ifreq in range(len(frequencies)):
             freq = frequencies[ifreq]
-            if not (freq in self.frequencies):
-                raise ValueError("Frequencies must be one of those" +\
-                                 " supplied with real data.")
             internal_ifreq = np.where(self.frequencies == freq)[0][0]
-            maps[ifreq,:] =\
-                maps_from_grids(self.grids[internal_ifreq,:,:], nside,\
-                theta_axis=-2, phi_axis=-1)
-            if not np.allclose(pointing, (90., 0.), atol=1e-5, rtol=0.):
-                maps[ifreq,:] = rotate_map(maps[ifreq,:], p_theta, p_phi, psi)
+            maps[ifreq,:] = self.maps[nside][internal_ifreq,:]
+        if (pointing != (90, 0)) or (psi != 0):
+            maps = rotate_maps(maps, 90 - pointing[0], pointing[1], psi,\
+                use_inverse=False, nest=False, axis=-1, deg=True, verbose=True)
         if normed:
-            maps = normalize_maps(maps)
-        if multi_frequency:
-            return maps[:,:]
-        else:
+            maps = normalize_maps(maps, pixel_axis=-1)
+        if (numfreqs == 1) and frequencies_originally_single_number:
             return maps[0,:]
+        else:
+            return maps[:,:]
     
     def decompose_spherical_harmonics(self, nside, lmax=None, group=True):
         """

perses/beam/total_power/IsotropicBeam.py
@@ -6,15 +6,47 @@ Date: 13 Dec 2019
 Description: File containing a class representing a theoretically perfectly
              achromatic and isotropic beam.
 """
+from __future__ import division
 from types import FunctionType
 import numpy as np
-from ...util import real_numerical_types
+import healpy as hp
+from ...util import real_numerical_types, bool_types
 from .IdealBeam import IdealBeam
 
 class IsotropicBeam(IdealBeam):
     """
     Class representing a theoretically perfectly achromatic and isotropic beam.
     """
+    def __init__(self, half_sky=False):
+        """
+        Initalizes a new isotropic beam over either 4pi or 2pi steradians.
+        
+        half_sky: if True, beam is over 2pi steradians, otherwise it is all 4pi
+        """
+        self.half_sky = half_sky
+    
+    @property
+    def half_sky(self):
+        """
+        Property storing whether this is an isotropic beam over only half the
+        sky or the whole sky.
+        """
+        if not hasattr(self, '_half_sky'):
+            raise AttributeError("half_sky was referenced before it was set.")
+        return self._half_sky
+    
+    @half_sky.setter
+    def half_sky(self, value):
+        """
+        Setter for the half_sky property.
+        
+        value: True or False
+        """
+        if type(value) in bool_types:
+            self._half_sky = value
+        else:
+            raise TypeError("half_sky was set to a non-bool.")
+    
     def beam_function(self, frequencies, thetas, phis):
         """
         The function which is called to modulate the dipole pattern. It is a
@@ -26,8 +58,11 @@ class IsotropicBeam(IdealBeam):
         NOTE: The three arguments to this function--frequencies, thetas, and
               phis--must all be castable into a common shape.
         """
-        return np.ones_like(frequencies) * np.ones_like(thetas) *\
-            np.ones_like(phis)
+        if self.half_sky:
+            theta_part = ((thetas < (np.pi / 2)) * 1.)
+        else:
+            theta_part = np.ones_like(thetas)
+        return np.ones_like(frequencies) * theta_part * np.ones_like(phis)
     
     def convolve(self, frequencies, sky_maps, pointing=(90, 0), psi=0,\
         func_pars={}, verbose=True, include_smearing=False, angles=None,\
@@ -84,8 +119,21 @@ class IsotropicBeam(IdealBeam):
             sky_maps = np.stack(sky_maps, axis=0)
         elif type(sky_maps) is not np.ndarray:
             raise TypeError("sky_maps given to convolve were not in " +\
-                            "sequence or function form.")
-        spectrum = np.mean(sky_maps, axis=1)
+                "sequence or function form.")
+        if horizon or self.half_sky:
+            npix = sky_maps.shape[1]
+            nside = hp.pixelfunc.npix2nside(npix)
+            angles_of_pixels = hp.pixelfunc.pix2ang(nside, np.arange(npix))
+            pointing_in_radians =\
+                (np.radians(90 - pointing[0]), np.radians(pointing[1]))
+            gammas = hp.rotator.angdist(angles_of_pixels, pointing_in_radians)
+            half_sky_mean = np.mean(sky_maps[:,gammas<(np.pi/2)], axis=1)
+            if horizon and (not self.half_sky):
+                spectrum = (ground_temperature + half_sky_mean) / 2
+            else:
+                spectrum = half_sky_mean
+        else:
+            spectrum = np.mean(sky_maps, axis=1)
         if len(angles) == 1:
             return spectrum
         else:

perses/simulations/DriftscanSetCreator.py
@@ -112,30 +112,6 @@ class DriftscanSetCreator(object):
             raise TypeError("file_name was set to a non-string.")
     
     @property
-    def observatory(self):
-        """
-        Property storing the GroundObservatory object describing the location
-        and orientation of the observatory on Earth.
-        """
-        if not hasattr(self, '_observatory'):
-            raise AttributeError("observatory referenced before it was set.")
-        return self._observatory
-    
-    @observatory.setter
-    def observatory(self, value):
-        """
-        Setter for the GroundObservatory object describing the location and
-        orientation of the observatory on Earth.
-        
-        value: a GroundObservatory object
-        """
-        if isinstance(value, GroundObservatory):
-            self._observatory = value
-        else:
-            raise TypeError("observatory was set to a " +\
-                "non-GroundObservatory object.")
-    
-    @property
     def observatory_function(self):
         """
         Property storing a function which generates the observatories used to
@@ -549,13 +525,14 @@ class DriftscanSetCreator(object):
         while 'observatory_0_beam_{:d}_maps_0'.format(nbeams) in group:
             nbeams += 1
         nmaps = 0
-        while 'beam_0_maps_{:d}'.format(nmaps) in group:
+        while 'observatory_0_beam_0_maps_{:d}'.format(nmaps) in group:
             nmaps += 1
         for iobservatory in range(nobservatories):
             for ibeam in range(nbeams):
                 for imaps in range(nmaps):
-                    these_spectra = get_hdf5_value(\
-                        group['beam_{0}_maps_{1}'.format(ibeam, imaps)])
+                    these_spectra = get_hdf5_value(group[\
+                        'observatory_{0:d}_beam_{1:d}_maps_{2:d}'.format(\
+                        iobservatory, ibeam, imaps)])
                     if (iobservatory == 0) and (ibeam == 0) and (imaps == 0):
                         training_set = np.ndarray(\
                             (nobservatories, nbeams, nmaps) +\