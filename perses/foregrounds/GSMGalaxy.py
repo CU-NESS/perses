Changed GSMGalaxy class to better conform to other SpatialPowerLawGalaxy subclasses. ; Fixed an issue with the gradient function of the FilterGainModel. ; Added a NonlinearReceiverFit class so that nonlinear signal models can be explored in receiver fits.

perses/foregrounds/GSMGalaxy.py
@@ -9,14 +9,15 @@ Description: File containing a class representing a Galaxy map with both
 
 Based on:
 
-Zheng, Tegmark, Dillon, Kim, Liu, Neben, Jonas, Reich, Reich 2017. An Improved Model
-of Diffuse Galactic Radio Emission from 10 MHz to 5 THz. MNRAS 464, 3486-3497.
+Zheng, Tegmark, Dillon, Kim, Liu, Neben, Jonas, Reich, Reich 2017. An Improved
+Model of Diffuse Galactic Radio Emission from 10 MHz to 5 THz. MNRAS 464,
+3486-3497.
 """
 from __future__ import division
 import os, time
 import numpy as np
 import healpy as hp
-from ..util import bool_types, real_numerical_types
+from ..util import bool_types
 from .SpatialPowerLawGalaxy import SpatialPowerLawGalaxy
 
 class GSMGalaxy(SpatialPowerLawGalaxy):
@@ -41,22 +42,20 @@ class GSMGalaxy(SpatialPowerLawGalaxy):
         verbose: if True, time it took to prepare the GSM map is printed
         """
         self.nside = nside
-        self.reference_frequency = reference_frequency
-        self.reference_map = self.gsm_map
-        self.spectral_index = spectral_index
-        self.thermal_background = thermal_background
         self.verbose = verbose
+        self.reference_frequency = reference_frequency
+        SpatialPowerLawGalaxy.__init__(self, self.gsm_map,\
+            self.reference_frequency, spectral_index,\
+            thermal_background=thermal_background)
 
     def create_map(self, low_resolution=True):
         """
         gsm_frequency: frequency of the desired GSM map in MHz
         low_resolution: desired resolution of the output gsm_map, either 
-                    low or high resolution.
-		    High resolution should be used for gsm_frequencies 
-		    above 10 GHz, otherwise low resolution is used as the 
-		    default.
-	
-	"""
+                        low or high resolution. High resolution should be used
+                        for gsm_frequencies above 10 GHz, otherwise low
+                        resolution is used as the default.
+        """
         input_path = '{!s}/input/gsm2016/data'.format(os.environ['PERSES'])
         labels = ['Synchrotron', 'CMB', 'HI', 'Dust1', 'Dust2', 'Free-Free']
         n_comp = len(labels)
@@ -70,14 +69,16 @@ class GSMGalaxy(SpatialPowerLawGalaxy):
         unit = 'TRJ'
 
         def K_RJ2MJysr(K_RJ, nu):#in Kelvin and Hz
-            conversion_factor = 2 * (nu / C)**2 * kB
-            return  K_RJ * conversion_factor * 1e20#1e-26 for Jy and 1e6 for MJy
+            conversion_factor = 2 * ((nu / C) ** 2) * kB
+            #1e20 below comes from 1e-26 for Jy and 1e6 for MJy
+            return  K_RJ * conversion_factor * 1e20
 
         if low_resolution == True:
             map_ni = np.loadtxt('{!s}/lowres_maps.txt'.format(input_path))
         else:
-            map_ni = np.array([np.fromfile('{0!s}/highres_{1!s}_map.bin'.format(\
-                input_path, lb), dtype='float32') for lb in labels])
+            map_ni = np.array([\
+                np.fromfile('{0!s}/highres_{1!s}_map.bin'.format(input_path,\
+                lb), dtype='float32') for lb in labels])
 
         spec_nf = np.loadtxt(input_path + '/spectra.txt')
         nfreq = spec_nf.shape[1]
@@ -100,7 +101,8 @@ class GSMGalaxy(SpatialPowerLawGalaxy):
         y2 = interp_spec_nf[1:, left_index + 1]
         x = np.log10(freq)
         interpolated_vals = (x * (y2 - y1) + x2 * y1 - x1 * y2) / (x2 - x1)
-        result = np.sum(10.**interpolated_vals[0] * (interpolated_vals[1:, None] * map_ni), axis=0)
+        result = np.sum(10.**interpolated_vals[0] *\
+            (interpolated_vals[1:, None] * map_ni), axis=0)
         conversion = 1. / K_RJ2MJysr(1., 1e9 * freq)
         result *= conversion
         result = hp.reorder(result, n2r=True)
@@ -113,27 +115,6 @@ class GSMGalaxy(SpatialPowerLawGalaxy):
         Returns 'GSM'
         """
         return 'GSM'
-    
-    @property
-    def reference_frequency(self):
-        """
-        Frequency at which map is created.
-        """
-        if not hasattr(self, '_reference_frequency'):
-            raise AttributeError("reference_frequency was referenced before it was set.")
-        return self._reference_frequency
-    
-    @reference_frequency.setter
-    def reference_frequency(self, value):
-        """
-        Setter for the reference_frequency property.
-        
-        value: positive number in MHz
-        """
-        if type(value) in real_numerical_types:
-            self._reference_frequency = value
-        else:
-            raise ValueError("reference_frequency was set to a non-number.")
     
     @property
     def verbose(self):
@@ -159,8 +140,8 @@ class GSMGalaxy(SpatialPowerLawGalaxy):
     @property
     def gsm_map(self):
         """
-        Property storing the GSM map at the reference frequency in MHz in Galactic coordinates at
-        native resolution.
+        Property storing the GSM map at the reference frequency in MHz in
+        Galactic coordinates at native resolution.
         """
         if not hasattr(self, '_gsm_map'):
             t1 = time.time()

perses/models/ButterworthFilterGainModel.py
@@ -69,7 +69,7 @@ class ButterworthFilterGainModel(FilterGainModelWithOrder):
         Computes and returns the derivatives of the prototype function at the
         given frequency ratios with respect to the prototype parameters.
         """
-        return np.zeros((0, self.num_channels))
+        return np.zeros((self.num_channels, 0))
     
     def base_function_frequency_derivative(self, x_values,\
         prototype_parameters):

perses/models/FilterGainModel.py
@@ -111,7 +111,7 @@ class FilterGainModel(LoadableModel):
         group: hdf5 group into which this model should be saved
         """
         create_hdf5_dataset(group, 'frequencies', data=self.frequencies)
-        group.attrs['kind'] = kind
+        group.attrs['kind'] = self.kind
     
     def fill_hdf5_group(self, group):
         """
@@ -263,7 +263,7 @@ class FilterGainModel(LoadableModel):
             prototype_gradient = self.base_function_parameter_gradient(\
                 x_values, other_parameters)
             return np.concatenate([prototype_gradient,\
-                [reference_frequency_derivative]], axis=0)
+                reference_frequency_derivative[:,np.newaxis]], axis=1)
         else:
             high_pass_other_parameters =\
                 parameters[:self.num_prototype_parameters]
@@ -293,12 +293,13 @@ class FilterGainModel(LoadableModel):
                 self.base_function_frequency_derivative(high_pass_x_values,\
                 high_pass_other_parameters) / self.frequencies
             low_pass_gradient = np.concatenate([low_pass_prototype_gradient,\
-                [low_pass_reference_frequency_derivative]], axis=0)
+                low_pass_reference_frequency_derivative[:,np.newaxis]], axis=1)
             high_pass_gradient = np.concatenate([high_pass_prototype_gradient,\
-                [high_pass_reference_frequency_derivative]], axis=0)
+                high_pass_reference_frequency_derivative[:,np.newaxis]],\
+                axis=1)
             return np.concatenate([\
-                high_pass_gradient * low_pass_part[np.newaxis,:],\
-                low_pass_gradient * high_pass_part[np.newaxis,:]], axis=0)
+                high_pass_gradient * low_pass_part[:,np.newaxis],\
+                low_pass_gradient * high_pass_part[:,np.newaxis]], axis=1)
     
     @property
     def hessian_computable(self):

perses/models/InverseChebyshevFilterGainModel.py
@@ -75,7 +75,7 @@ class InverseChebyshevFilterGainModel(FilterGainModelWithOrder):
         """
         gain = self.base_function(x_values, prototype_parameters)
         return\
-            ((2 / prototype_parameters[0]) * gain * (1 - gain))[np.newaxis,:]
+            ((2 / prototype_parameters[0]) * gain * (1 - gain))[:,np.newaxis]
     
     def base_function_frequency_derivative(self, x_values,\
         prototype_parameters):

perses/receiver_fits/NonlinearReceiverFit.py
@@ -1,5 +1,5 @@
 """
-File: perses/receiver_fits/LinearReceiverFit.py
+File: perses/receiver_fits/NonlinearReceiverFit.py
 Author: Keith Tauscher
 Date: 30 Jan 2020
 
@@ -11,11 +11,11 @@ Description: File containing class representing a fit with a receiver where the
 from __future__ import division
 import numpy as np
 import scipy.linalg as scila
-from distpy import GaussianDistribution, DistributionSet
-from pylinex import BasisModel
+from distpy import Distribution, GaussianDistribution, DistributionSet
+from pylinex import Model
 from .ReceiverFit import ReceiverFit
 
-class LinearReceiverFit(ReceiverFit):
+class NonlinearReceiverFit(ReceiverFit):
     """
     Class representing a fit with a receiver where the foreground, receiver
     offset, and signal all have linear models. In this case, the posterior can
@@ -27,7 +27,7 @@ class LinearReceiverFit(ReceiverFit):
         Property storing the knowledge that signal_model must be a BasisModel
         for this ReceiverFit.
         """
-        return BasisModel
+        return Model
     
     @property
     def signal_prior_class(self):
@@ -35,7 +35,7 @@ class LinearReceiverFit(ReceiverFit):
         Property storing the knowledge that signal_prior must be a
         GaussianDistribution for this ReceiverFit.
         """
-        return GaussianDistribution
+        return Distribution
     
     @property
     def marginalized_name_chains(self):
@@ -44,8 +44,8 @@ class LinearReceiverFit(ReceiverFit):
         marginalized over.
         """
         if not hasattr(self, '_marginalized_name_chains'):
-            self._marginalized_name_chains = [['from_antenna', name]\
-                for name in ['signal', 'foreground', 'offset']]
+            self._marginalized_name_chains =\
+                [['from_antenna', name] for name in ['foreground', 'offset']]
         return self._marginalized_name_chains
     
     @property
@@ -57,7 +57,7 @@ class LinearReceiverFit(ReceiverFit):
         """
         if not hasattr(self, '_marginalized_priors'):
             self._marginalized_priors =\
-                [self.signal_prior, self.foreground_prior, self.offset_prior]
+                [self.foreground_prior, self.offset_prior]
         return self._marginalized_priors
     
     @property
@@ -67,9 +67,13 @@ class LinearReceiverFit(ReceiverFit):
         performed for this fit.
         """
         if not hasattr(self, '_prior_distribution_set'):
-            self._prior_distribution_set = DistributionSet([(self.gain_prior,\
+            self._prior_distribution_set = DistributionSet()
+            self._prior_distribution_set.add_distribution(self.gain_prior,\
                 ['gain_{!s}'.format(parameter)\
-                for parameter in self.gain_model.parameters])])
+                for parameter in self.gain_model.parameters])
+            self._prior_distribution_set.add_distribution(self.signal_prior,\
+                ['from_antenna_signal_{!s}'.format(parameter)\
+                for parameter in self.signal_model.parameters])
         return self._prior_distribution_set
     
     def _build_sample(self):
@@ -84,20 +88,21 @@ class LinearReceiverFit(ReceiverFit):
         self._offset_parameter_sample = []
         self._foreground_parameter_sample = []
         self._signal_parameter_sample = []
-        signal_slice = slice(None, self.signal_model.num_parameters)
-        foreground_slice = slice(self.signal_model.num_parameters,\
-            -self.offset_model.num_parameters)
+        gain_slice = slice(None, self.gain_model.num_parameters)
+        signal_slice = slice(-self.signal_model.num_parameters, None)
+        foreground_slice = slice(None, self.foreground_model.num_parameters)
         offset_slice = slice(-self.offset_model.num_parameters, None)
-        running_covariance = np.zeros((self.signal_model.num_parameters +\
-            self.foreground_model.num_parameters +\
+        running_covariance = np.zeros((self.foreground_model.num_parameters +\
             self.offset_model.num_parameters,) * 2)
         num_conditional_distributions = 0
         full_means = []
         for element in np.reshape(self.fitter.chain[:,::thin,:],\
-            (-1, self.gain_model.num_parameters)):
-            #self._gain_parameter_sample.append(\
-            #    element[np.newaxis,:] * np.ones((samples_per_element, 1)))
-            self._gain_parameter_sample.append(element[np.newaxis,:])
+            (-1, self.gain_model.num_parameters +\
+            self.signal_model.num_parameters)):
+            gain_element = element[gain_slice]
+            signal_element = element[signal_slice]
+            self._gain_parameter_sample.append(gain_element[np.newaxis,:])
+            self._signal_parameter_sample.append(signal_element[np.newaxis,:])
             (recreation, conditional_mean, conditional_covariance) =\
                 self.conditional_fit_model(element,\
                 return_conditional_mean=True,\
@@ -108,17 +113,17 @@ class LinearReceiverFit(ReceiverFit):
             num_conditional_distributions += 1
             conditional_distribution =\
                 GaussianDistribution(conditional_mean, conditional_covariance)
-            sfo_sample = conditional_distribution.draw(samples_per_element)
-            self._signal_parameter_sample.append(sfo_sample[:,signal_slice])
+            fo_sample = conditional_distribution.draw(samples_per_element)
             self._foreground_parameter_sample.append(\
-                sfo_sample[:,foreground_slice])
-            self._offset_parameter_sample.append(sfo_sample[:,offset_slice])
+                fo_sample[:,foreground_slice])
+            self._offset_parameter_sample.append(fo_sample[:,offset_slice])
         running_covariance = running_covariance / num_conditional_distributions
         # now, running_covariance is the component of sfo covariance due solely
         # to the sizes/shapes of the conditional distributions of sfo at
         # constant gain
         running_covariance = scila.block_diag(np.zeros(\
-            (self.gain_model.num_parameters,) * 2), running_covariance)
+            (self.gain_model.num_parameters +\
+            self.signal_model.num_parameters,) * 2), running_covariance)
         # now, running_covariance is the component of the full (gsfo)
         # covariance due solely to the sizes/shapes of the conditional
         # distributions of sfo at constant gain

perses/receiver_fits/ReceiverFit.py
@@ -14,7 +14,8 @@ import matplotlib.pyplot as pl
 from matplotlib.colors import SymLogNorm
 from distpy import triangle_plot, Distribution, GaussianDistribution
 from pylinex import Model, BasisModel, SumModel, ProductModel,\
-    ConditionalFitGaussianLoglikelihood, Sampler, BurnRule, NLFitter
+    MultiConditionalFitModel, ConditionalFitGaussianLoglikelihood, Sampler,\
+    BurnRule, NLFitter
 from ..util import sequence_types, real_numerical_types
 
 try:
@@ -369,13 +370,23 @@ class ReceiverFit(object):
         beam-weighted foreground, and signal.
         """
         if not hasattr(self, '_full_model'):
-            ideal_model = SumModel(['signal', 'foreground'],\
-                [self.signal_model, self.foreground_model])
-            multiplicative_model =\
-                ProductModel(['gain', 'ideal'], [self.gain_model, ideal_model])
-            self._full_model = SumModel(['multiplicative', 'offset'],\
-                [multiplicative_model, self.offset_model])
+            from_antenna_model = SumModel(['signal', 'foreground', 'offset'],\
+                [self.signal_model, self.foreground_model, self.offset_model])
+            self._full_model = ProductModel(['gain', 'from_antenna'],\
+                [self.gain_model, from_antenna_model])
         return self._full_model
+    
+    @property
+    def conditional_fit_model(self):
+        """
+        Property storing the ConditionalFitModel that performs the
+        marginalization.
+        """
+        if not hasattr(self, '_conditional_fit_model'):
+            self._conditional_fit_model = MultiConditionalFitModel(\
+                self.full_model, self.data, self.error,\
+                self.marginalized_name_chains, priors=self.marginalized_priors)
+        return self._conditional_fit_model
     
     @property
     def conditional_fit_gaussian_loglikelihood(self):
@@ -490,13 +501,20 @@ class ReceiverFit(object):
         """
         return self.fitter.plot_chain(**kwargs)
     
-    def plot_full_covariance(self, normalize_by_variances=False, fig=None,\
-        ax=None, title=None, fontsize=24, show=False, **kwargs):
+    def plot_full_covariance(self, normalize_by_variances=False,\
+        correction_factors=None, fig=None, ax=None, title=None, fontsize=24,\
+        show=False, **kwargs):
         """
         Plots the full covariance (or correlation) matrix.
         
         normalize_by_variances: if True, the correlation matrix is plotted
                                 instead of the covariance matrix
+        correction_factors: (only used if normalize_by_variances is False) a 1D
+                            array of numbers by which to multiply the standard
+                            deviations of each parameter. Each row will be
+                            divided by the corresponding element of this
+                            array and each column will be divided by the
+                            corresponding element of this array
         fig: matplotlib Figure on which to plot if it already exists (only used
              if ax is None)
         ax: matplotlib Axes on which to plot if it already exists
@@ -529,7 +547,11 @@ class ReceiverFit(object):
         if normalize_by_variances:
             image = ax.imshow(self.full_correlation, **imshow_kwargs)
         else:
-            image = ax.imshow(self.full_covariance, **imshow_kwargs)
+            to_plot = self.full_covariance
+            if type(correction_factors) is not type(None):
+                to_plot = to_plot / (correction_factors[:,np.newaxis] *\
+                    correction_factors[np.newaxis,:])
+            image = ax.imshow(to_plot, **imshow_kwargs)
         cbar = pl.colorbar(image)
         limits = [-0.5, self.full_model.num_parameters - 0.5]
         for number in np.cumsum([self.gain_model.num_parameters,\
@@ -725,8 +747,8 @@ class ReceiverFit(object):
             return fig
     
     def plot_offset_curve_sample(self, input_curve=None, x_values=None,\
-        further_thin=100, alpha=0.05, xlabel='$x$', ylabel='$O$ [mK]',\
-        scale_factor=1e3, residual_ylabel='$O-<O>$ [mK]',\
+        further_thin=100, alpha=0.05, xlabel='$x$', ylabel='$O$ [K]',\
+        scale_factor=1, residual_ylabel='$O-<O>$ [mK]',\
         residual_scale_factor=1e3, title='Offset curve sample', fig=None,\
         fontsize=24, show=False):
         """
@@ -1075,18 +1097,24 @@ class ReceiverFit(object):
         raise cannot_instantiate_error
     
     @property
-    def conditional_fit_model(self):
-        """
-        Property storing the ConditionalFitModel that performs the
-        marginalization.
-        """
-        raise cannot_instantiate_error
-    
-    @property
     def prior_distribution_set(self):
         """
         Property storing the prior distribution set for the MCMC that will be
         performed for this fit.
+        """
+        raise cannot_instantiate_error
+    
+    @property
+    def marginalized_name_chains(self):
+        """
+        Property storing the name chains to marginalize in this fit.
+        """
+        raise cannot_instantiate_error
+    
+    @property
+    def marginalized_priors(self):
+        """
+        Property storing the priors on the marginalized components of this fit.
         """
         raise cannot_instantiate_error
 